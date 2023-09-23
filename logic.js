80750e106fa55db830a8b666621a0d239b93f4b0
dhananjaya5023 <dhananjaya5023@gmail.com>
2023-09-23 10:55:48 -0400

    logic

---
+++ logic.js
@@ -0,0 +1,11 @@
+document.write("<p>A=true,B=false</p>");
+ 
+var a=true;
+var b=false;
+ 
+document.write("<br/>");
+document.write("logical A&&B :"+(a&&b));
+document.write("<br/>");
+document.write("logical A||B :"+(a||b));
+document.write("<br/>");
+document.write("loical  A!B :"+(!b))
